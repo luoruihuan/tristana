2233490564fdc08fcc4a6acecc013b74ac9e5bde
xuya227939 <hezhiqianye@gmail.com>
2020-06-16 22:22:40 +0800

    fix: fix socket package

--- src/components/Socket/index.jsx
+++ src/components/Socket/index.jsx
@@ -4,10 +4,10 @@
  * @Author: Jiang
  * @Date: 2019-08-27 18:00:15
  * @Last Modified by: Jiang
- * @Last Modified time: 2020-06-16 22:15:34
+ * @Last Modified time: 2020-06-16 22:19:15
  */
 
-import io from 'socket.io';
+import io from 'socket.io-client';
 
 let SOCKET = '';
 
