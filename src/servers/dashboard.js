1c5fe90bd7b68e3540b03909980675ee59aff2ea
发声的沉默者 <hezhiqianye@gmail.com>
2020-08-19 21:53:40 +0800

    Update dashboard.js

--- src/servers/dashboard.js
+++ src/servers/dashboard.js
@@ -1,10 +1,9 @@
 import request from 'request';
 
 // 获取表格数据
-export function getTable(params = {}) {
+export function getTable() {
     return request({
-        url: 'getTable',
-        method: 'POST',
-        data: params
+        url: 'getTable.json',
+        method: 'GET'
     });
 }
